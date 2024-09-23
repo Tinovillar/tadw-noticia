caa756374e976e2ccc92bd618dd36dda38b9927d
Tino <valennvillar@gmail.com>
2024-09-23 19:58:25 -0300

    toast

--- src/CompCiencia.js
+++ src/CompCiencia.js
@@ -4,7 +4,7 @@ function CompCiencia() {
     // return a jsx ul list of a menu with five random items
     return  (<div>
                 <h4>Noticia de Ciencia</h4>
-                <p>El Departamento de Ciencias e Ingeniería de la Computación (DCIC) sorprendió al mundo del desarrollo de software al presentar un nuevo lenguaje de programación llamado ToastScript, diseñado específicamente para automatizar el proceso de tostar pan. Según los desarrolladores, este innovador lenguaje permitirá optimizar la cocción perfecta del pan mediante algoritmos avanzados de aprendizaje automático. "ToastScript integra inteligencia artificial con sensores de calor en tiempo real, permitiendo a los usuarios ajustar el nivel de dorado en milisegundos", comentó la ingeniera principal, Dra. Panini. El DCIC asegura que esta tecnología revolucionará tanto el desayuno como la industria del software, prometiendo actualizaciones que permitirán a las tostadoras compilar código directamente.</p>
+                <p>El Departamento de Ciencias e Ingeniería de la Computación (DCIC) sorprendió al mundo del desarrollo de software al presentar un nuevo lenguaje de programación llamado ToastScript, diseñado específicamente para automatizar el proceso de tostar pan.</p>
             </div>);
 }
 
