edb8321c30ee572641dc51742c322d266fca5f81
Tino <valennvillar@gmail.com>
2024-09-23 19:54:54 -0300

    app-tadw

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CompCiencia from "./CompCiencia";
+
+const App = () => (
+  <div className="container">
+     <CompCiencia />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
+
+  
